dfc3ae420025c1141ccb2d663f8df6ce5662e491
Yash Virsodiya <virsodiyayash4@gmail.com>
2024-09-09 00:47:08 +0530

    Changes in Queue

--- Project - 01 (DS in WEB)/Queue/app.js
+++ Project - 01 (DS in WEB)/Queue/app.js
@@ -1,9 +1,13 @@
 const select = document.getElementById('function');
 const parameter = document.getElementById('parameter');
-const statusbox = document.getElementById('status');
+const statusbox = document.querySelector('.status');
 const queueBox = document.getElementById('queue');
 let functionCode = 0;
-let queue = [];
+let queuelist = [];
+let nodeCountInRow = 5;
+let width = 0;
+let innerHeight = 20;
+let functionWorking = false;
 
 print();
 
@@ -11,19 +15,27 @@ print();
 select.addEventListener('change' , (e)=>{
     parameter.innerHTML = functionParameter[select.value];
     functionCode = parseInt(select.value);
+    let value = document.getElementById('data');
+    if(value != null){
+        value.focus();
+    }
     let index = document.getElementById('index');
     if(index != null){
         index.focus();
-        return;
     }
-    let value = document.getElementById('data');
-    value.focus();
 });
 
 
 
 // For calling different function.
-async function enter(){
+async function runSelectedFunction(){
+    if( functionWorking ) return;
+    functionWorking = true;
+
+    let value = document.getElementById('data');
+    if(value != null) value.blur();
+    let index = document.getElementById('index');
+    if(index != null) index.blur();
 
     switch(functionCode){
         case 0 :
@@ -32,37 +44,40 @@ async function enter(){
         case 1 :
             await insertAtEnd();
             break;
-        case 2 :
-            await deleteByFirst();
-            break;
-        case 3 :
-            await insertAtFirst();
-            break;
-        case 4 :
-            await deleteByLast();
-            break;
-        // case 5 :
-        //     await deleteByData();
+        // case 2 :
+        //     await insertAtFirst();
         //     break;
+        // case 3 :
+        //     await insertAtIndex();
+        //     break;
+        // case 4 :
+        //     await deleteByIndex();
+        //     break;
+        case 5 :
+            await deleteByData();
+            break;
     }
-    let value = document.getElementById('data');
-    if(value!= null){
+    
+    if(value != null){
         value.value = '';
         value.focus();
     }
-    let index = document.getElementById('index');
     if(index != null){
         index.value = '';
         index.focus();
     }
+    functionWorking = false;
 }
 
 
 
 // changeStatus function Update the status in div having status class.
-function changeStatus(text, color){
+async function changeStatus(text, color){
     statusbox.style.color = color;
     statusbox.innerHTML = text;
+    statusbox.classList.add('statusBlink');
+    await delay(300);
+    statusbox.classList.remove('statusBlink');
 }
 
 
@@ -77,113 +92,27 @@ async function insertAtEnd(){
     }
     data = parseInt(data.value);
 
-    for(let i = 0 ; i < linkedlist.length ; i++){
-        document.querySelector('._' + i).classList.add('current');
-        await delay(500);
-        document.querySelector('._' + i).classList.remove('current');
+    if(data < -999 || data > 999){
+        changeStatus("Range of value is -999 to 999", 'red');
+        return;
     }
 
-    linkedlist.push(data);
+    await travelToIndex(queuelist.length);
+
+    queuelist.push(data);
     print();
-    document.querySelector('._' + (linkedlist.length - 1) ).classList.add('fade-In');
+    scrollTo( document.querySelector('._' + (queuelist.length - 1) ) );
+    document.querySelector('._' + (queuelist.length - 1) ).classList.add('fade-In');
     changeStatus('Node is Successfully Added', 'green');
+    await delay(500);
+    document.querySelector('._' + (queuelist.length - 1)).classList.remove('fade-In');
 }
-
-
-
-// The insertAtFirst function adds a node at the start of the linked list
-// Also update the div with class 'linkedlist' and the div with class 'status'.
-async function insertAtFirst(){
-    let data = document.getElementById('data');
-    if(data.value == ''){
-        changeStatus("Enter Parameter", 'red');
-        return;
-    }
-    data = parseInt(data.value);
-    linkedlist.unshift(data);
-    print();
-    document.querySelector('._0').classList.add('fade-In');
-    changeStatus('Node is Successfully Added', 'green');
-}
-
-
-
-// The insertAtIndex function adds a node at the index specified by the user
-// Also update the div with class 'linkedlist' and the div with class 'status'.
-async function insertAtIndex(){
-    let data = document.getElementById('data');
-    let index = document.getElementById('index');
-    if(data.value == '' || index.value == ''){
-        changeStatus("Enter Parameter", 'red');
-        return;
-    }
-    index = parseInt(index.value);
-
-    if(index < 0){
-        changeStatus('Index can\'t be negative', 'red');
-        return;
-    }
-
-    for(let i = 0 ; i < index && i < linkedlist.length ; i++){
-        document.querySelector('._' + i).classList.add('current');
-        await delay(500);
-        document.querySelector('._' + i).classList.remove('current');
-    }
-
-    if(index > linkedlist.length){
-        changeStatus('Index ' + index + ' doesn\'t exist', 'red');
-        return;
-    }
-    data = parseInt(data.value);
-    linkedlist.splice( index, 0, data);
-    print();
-    document.querySelector('._' + index).classList.add('fade-In');
-    changeStatus('Node is Successfully Added', 'green');
-}
-
-
-
-// The deleteByIndex function deletes the node at the given index
-// Also update the div with class 'linkedlist' and the div with class 'status'.
-async function deleteByIndex(){
-    if(linkedlist.length == 0){
-        changeStatus("Linked List is empty", 'red');
-        return;
-    }
-    let index = document.getElementById('index');
-    if(index.value == ''){
-        changeStatus("Enter Parameter", 'red');
-        return;
-    }
-    index = parseInt(index.value);
-
-    if(index < 0){
-        changeStatus('Index can\'t be negative', 'red');
-        return;
-    }
-
-    for(let i = 0 ; i < index && i < linkedlist.length ; i++){
-        document.querySelector('._' + i).classList.add('current');
-        await delay(500);
-        document.querySelector('._' + i).classList.remove('current');
-    }
-
-    if(index >= linkedlist.length ){
-        changeStatus('Index ' + index + ' doesn\'t exist', 'red');
-        return;
-    }
-    document.querySelector('._' + index).classList.add('fade-Out');
-    linkedlist.splice(index, 1);
-    setTimeout( print , 500);
-    changeStatus('Node is Successfully Deleted', 'green');
-}
-
 
 
 // The deleteByData function deletes the node that contains the specified data.
 // Also update the div with class 'linkedlist' and the div with class 'status'.
 async function deleteByData(){
-    if(linkedlist.length == 0){
+    if(queuelist.length == 0){
         changeStatus("Linked List is empty", 'red');
         return;
     }
@@ -193,52 +122,115 @@ async function deleteByData(){
         return;
     }
     data = parseInt(data.value);
-    let index = -1;
 
-    for(let i = 0 ; i < linkedlist.length ; i++){
-        document.querySelector('._' + i).classList.add('current');
-        await delay(500);
-        document.querySelector('._' + i).classList.remove('current');
-        if(linkedlist[i] == data){
-            index = i;
-            break;
-        }
+    if(data < -999 || data > 999){
+        changeStatus("Range of value is -999 to 999", 'red');
+        return;
     }
 
+    let index = queuelist.findIndex(d => d == data);
+
     if(index == -1){
+        await travelToIndex(queuelist.length);
         changeStatus('The linked list doesn\'t contain the value ' + data, 'orange');
         return;
     }
 
+    await travelToIndex(index);
+    scrollTo( document.querySelector('._' + index) );
     document.querySelector('._' + index).classList.add('fade-Out');
-    linkedlist.splice(index, 1);
+    queuelist.splice(index, 1);
     setTimeout( print , 500);
     changeStatus('Node is Successfully Deleted', 'green');
 }
 
 
+async function travelToIndex(index){
+    for(let i = 0 ; i < index && i < linkedlist.length ; i++){
+        scrollTo( document.querySelector('._' + i) );
+        document.querySelector('._' + i).classList.add('current');
+        await delay(150);
+        document.querySelector('._' + i).classList.remove('current');
+        document.querySelector('.a' + i).classList.add('currentArrow');
+        await delay(150);
+        document.querySelector('.a' + i).classList.remove('currentArrow');
+    }
+}
+
+
+async function scrollTo(element){
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',    
+        inline: 'nearest' 
+    });
+}
+
+
+// The createElementWith function create element with given tag, classes, id, content and return it. 
+function createElementWith(HTMLtag, allClass, id, content){
+    let element = document.createElement(HTMLtag);
+    for(let i = 0 ; i < allClass.length ; i++){
+        element.classList.add(allClass[i]);
+    }
+    element.id = id;
+    element.textContent = content;
+    return element;
+}
+
+
+// The createRow function create row with default parameters and return it. 
+function createRow(count , current){
+    const row = createElementWith('div', ['row'], null, "");
+    row.style.flexDirection = ( (current / count) % 2 == 0 ) ? "row" : "row-reverse" ;
+    row.style.padding = ( (current / count) % 2 == 0 ) ? "0px " + innerHeight / 2 + "px 0px 0px" : "0px 0px 0px " + innerHeight / 2 + "px" ;
+    return row;
+}
 
 // The print function updated content of the div with class 'linkedlist'.
 function print(){
-    linkedlistBox.innerHTML = "";
+    queueBox.innerHTML = "";
+    let current = 0;
+    let count = nodeCountInRow;
 
+    while(current < queuelist.length){
+        const row = createRow(count , current);
+        var arrowClass = ( (current / count) % 2  == 0 ) ? 'fa-arrow-right-long' : 'fa-arrow-left-long';
+        let i;
+        for(i = 0 ; current < queuelist.length && i < count ; i++){
+            const node = createElementWith('div', ['node', '_' + current], null, queuelist[current]);
+            const link = createElementWith('i', ['fa-solid', arrowClass, 'a' + current], null, "");   
+            
+            row.appendChild(node);
+            row.appendChild(link);
+            current++;
+        }
+        if(i == count){
+            row.lastChild.remove();
+            if((current / count) % 2 == 1) row.innerHTML += '<i class="fa-solid fa-arrow-turn-down ' + 'a' + (current - 1) + '" style=" transform: translateY(50%) "></i>';
+            else row.innerHTML += '<i class="fa-solid fa-arrow-turn-up ' + 'a' + (current - 1) + '" style=" transform: translateY(50%) rotateZ(180deg) "></i>';
+        }
+        queueBox.appendChild(row);
+    }   
     
-    for(let i = 0 ; i < linkedlist.length ; i++){
-        const node = document.createElement('div');
-        const link = document.createElement('i');
-        
-        node.classList.add('node');
-        node.textContent = linkedlist[i];
-        node.classList.add('_' + i);
-        
-        link.classList.add('fa-solid');
-        link.classList.add('fa-arrow-right-long');    
-
-        linkedlistBox.appendChild(node);
-        linkedlistBox.appendChild(link);
+    if(current % count == 0) {
+        const row = createRow(count , current);
+        queueBox.appendChild(row);
     }
     
-    linkedlistBox.innerHTML += 'NULL';
+    const node = createElementWith('div', ['node', 'null'], null, "Null");
+    queueBox.lastChild.appendChild(node);
+    current++;
+    
+    while(current % count != 0){
+        const node = createElementWith('div', ['node', 'null'], null, "");
+        queueBox.lastChild.appendChild(node);
+        current++;
+    }
+    
+    width = queueBox.clientWidth;
+    innerHeight = document.querySelector('.row').clientHeight;
+    nodeCountInRow = Math.floor( (width - (innerHeight) ) / ( (innerHeight * 1.5 ) ) );
 }
 
 
@@ -257,4 +249,10 @@ document.getElementById('showCode').addEventListener('click', (e)=>{
 
 document.getElementById('close').addEventListener('click', ()=>{
     document.getElementById('background').style.visibility = 'hidden';
+});
+
+// The below EventListeners is for some shortcuts.
+document.addEventListener('keypress',(e) => {
+    if(e.code == 'KeyF') select.focus();
+    else if(e.code == 'Enter') runSelectedFunction();
 });
--- Project - 01 (DS in WEB)/Queue/function.js
+++ Project - 01 (DS in WEB)/Queue/function.js
@@ -1,8 +1,8 @@
 const functionParameter = [
     "<div class='innerbox'><div style='font-size: 150%;'>Parameters</div></div>",
     "<div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>",
-    "<div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>",
-    "<div class='innerbox'><div class='text'>Index : </div><input type='number' class='input' id='index'></div><div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>",
-    "<div class='innerbox'><div class='text'>Index : </div><input type='number' class='input' id='index'></div>",
-    "<div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>"
+    "<div class='innerbox'><div class='text'> () </div></div>",
+    // "<div class='innerbox'><div class='text'>Index : </div><input type='number' class='input' id='index'></div><div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>",
+    // "<div class='innerbox'><div class='text'>Index : </div><input type='number' class='input' id='index'></div>",
+    // "<div class='innerbox'><div class='text'>Value : </div><input type='number' class='input' id='data'></div>"
 ];
